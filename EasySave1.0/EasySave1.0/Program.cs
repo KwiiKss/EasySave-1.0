994bd8f505a1e6d6796d7c3310f0883ed72b821f
KwiiKss <lrent.kevin@gmail.com>
2022-11-25 12:57:18 +0100

    update / delete useless code

--- EasySave1.0/EasySave1.0/Program.cs
+++ EasySave1.0/EasySave1.0/Program.cs
@@ -16,37 +16,6 @@ namespace EasySave
         {
             ViewEN myViewEN = new ViewEN();
             myViewEN.Start();
-
-            string pathes = @"C:\Users\Corso\Desktop\Dossier1\test.txt";
-            string path = @"C:\Users\Corso\Desktop\Dossier2\test.txt";
-            string paths = @"C:\Users\Corso\Desktop\Dossier2\LOGtest.txt";
-            FileInfo fInfo = new FileInfo(pathes);
-            float size = fInfo.Length;//taille en octets
-            var sw = new Stopwatch();
-            try
-            {
-                sw.Start();
-                File.Move(pathes, path);
-                Console.WriteLine("Moved");
-                sw.Stop();
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine(ex);
-            }
-            var json = new Json()
-            {
-                Name = "test.txt",
-                FileSource = pathes,
-                FileTarget = path,
-                DestPath = "",
-                FileSize = size,
-                FileTransferTime = sw.ElapsedMilliseconds,
-                Time = DateTime.Now.ToString("dd/M/y HH:mm:ss")
-            };
-            string jsonString = JsonSerializer.Serialize(json);
-            Model myModel = new Model();
-            myModel.Move(jsonString, paths);
         }
     }
 }
