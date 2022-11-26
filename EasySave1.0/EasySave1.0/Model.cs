35dad2aab7e932bcb6a882a0a3d917dbdf90bbdf
kilyion <kilyion.romary@viacesi.fr>
2022-11-26 13:14:35 +0100

    Easysave v1

--- EasySave1.0/EasySave1.0/Model.cs
+++ EasySave1.0/EasySave1.0/Model.cs
@@ -10,6 +10,7 @@ using static System.Console;
 
 namespace EasySave
 {
+
     class Model
     {
         public static void CreateFolder()
@@ -51,7 +52,6 @@ namespace EasySave
             ReadKey(true);
             ViewFR.RunMainMenuFR();
         }
-
         public static void ShowFolders() // Method that allow the user to check a list of all the files or folder in a searched folder
         {
             Console.Clear();
@@ -79,11 +79,11 @@ namespace EasySave
             ReadKey(true);
             ViewFR.RunMainMenuFR();
         }
-
         public static void MoveFile() // Method that moves a file form a source folder to a destination folder
         {
+            Model json = new Model();
+            var sw = new Stopwatch();
             Console.Clear();
-
             Console.WriteLine("Enter your file with his type (ex : kilyion.txt) :");
             string file = Console.ReadLine();
             Console.WriteLine("Enter the source of your file:");
@@ -92,19 +92,24 @@ namespace EasySave
             Console.WriteLine("Enter the destination of your file :");
             string dest = Console.ReadLine();
             string destFileName = dest + "\\" + file + "";
-
+            FileInfo fInfo = new FileInfo(sourceFileName);
+            float size = fInfo.Length;
+            sw.Start();
             File.Move(sourceFileName, destFileName);
+            sw.Stop();
+            string Text = json.SetJson(sourceFileName, destFileName, size, sw.ElapsedMilliseconds);
+            json.FileLog(Text);
             WriteLine($"\n-> The file moved in {destFileName}.");
             WriteLine("\n\nPress any key to return to the menu...");
-
             ReadKey(true);
             ViewEN.RunMainMenuEN();
         }
 
         public static void MoveFolder()  // Method that moves a folder form a source folder to a destination folder
         {
+            Model json = new Model();
+            var sw = new Stopwatch();
             Console.Clear();
-
             Console.WriteLine("Enter your folder name :");
             string folder = Console.ReadLine();
             Console.WriteLine("Enter the source of your folder :");
@@ -113,19 +118,18 @@ namespace EasySave
             Console.WriteLine("Enter the destination of your folder :");
             string dest = Console.ReadLine();
             string destFolderName = dest + "\\" + folder + "";
-
             Directory.Move(sourceFolderName, destFolderName);
             WriteLine($"\n-> The folder moved in {destFolderName}.");
             WriteLine("\n\nPress any key to return to the menu...");
-
             ReadKey(true);
             ViewEN.RunMainMenuEN();
         }
 
         public static void DeplacerFichier() // Méthode qui déplace un fichier d'un dossier source à un dossier destination
         {
+            Model json = new Model();
+            var sw = new Stopwatch();
             Console.Clear();
-
             Console.WriteLine("Entrez le nom du fichier avec son type (ex : kilyion.txt) :");
             string file = Console.ReadLine();
             Console.WriteLine("Entrez la source du fichier:");
@@ -134,18 +138,22 @@ namespace EasySave
             Console.WriteLine("Entrez la destination du fichier :");
             string dest = Console.ReadLine();
             string destFileName = dest + "\\" + file + "";
-
+            FileInfo fInfo = new FileInfo(sourceFileName);
+            float size = fInfo.Length;
+            sw.Start();
             File.Move(sourceFileName, destFileName);
+            sw.Stop();
+            string Text = json.SetJson(sourceFileName, destFileName, size, sw.ElapsedMilliseconds);
+            json.FileLog(Text);
             WriteLine($"\n-> Le fichier a bien été déplacé dans {destFileName}.");
-            WriteLine("\n\nAppuyez sur n'importe quelle touche pour revenir au menu...");
-
+            WriteLine("\n\nAppuyez sur n'importe quel bouton pour revenir au menu...");
             ReadKey(true);
-            ViewFR.RunMainMenuFR();
+            ViewEN.RunMainMenuEN();
         }
         public static void DeplacerDossier()  // Méthode qui déplace un dossier ainsi que son contenu d'une source à une destination
         {
-            Console.Clear();
-
+            Model json = new Model();
+            var sw = new Stopwatch();
             Console.WriteLine("Entrez le nom du dossier :");
             string folder = Console.ReadLine();
             Console.WriteLine("Entrez la source du dossier:");
@@ -154,13 +162,107 @@ namespace EasySave
             Console.WriteLine("Entrez la destination du dossier :");
             string dest = Console.ReadLine();
             string destFolderName = dest + "\\" + folder + "";
-
+            FileInfo fInfo = new FileInfo(sourceFolderName);
+            float size = fInfo.Length;
+            sw.Start();
             Directory.Move(sourceFolderName, destFolderName);
+            sw.Stop();
+            string Text = json.SetJson(sourceFolderName, destFolderName, size, sw.ElapsedMilliseconds);
+            json.FileLog(Text);
             WriteLine($"\n-> Le dossier a bien été déplacé dans {destFolderName}.");
-            WriteLine("\n\nAppuyez sur n'importe quelle touche pour revenir au menu...");
-
+            WriteLine("\n\nAppuyez sur n'importe quel bouton pour revenir au menu...");
             ReadKey(true);
-            ViewFR.RunMainMenuFR();
+            ViewEN.RunMainMenuEN();
         }
+
+        public class Json
+        {
+            public String name { get; set; }
+            public String FileSource { get; set; }
+            public String FileTarget { get; set; }
+            public String destPath { get; set; }
+            public float FileSize { get; set; }
+            public long FileTransferTime { get; set; }
+            public String time { get; set; }
+
+
+        }
+
+        public string SetJson(string path, string despath, float size, long time)
+        {
+            var option = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var json = new Json
+            {
+                name = "Move",
+                FileSource = path,
+                FileTarget = despath,
+                destPath = "",
+                FileSize = size,
+                FileTransferTime = time,
+                time = DateTime.Now.ToString("dd/M/y HH:mm:ss")
+            };
+            string jsonString = JsonSerializer.Serialize(json);
+            return jsonString;
+        }
+        public void FileLog(string json)
+        {
+            string path = @"E:\A3\Prog Système\Projet\tests\Log";
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    // Create the file, or overwrite if the file exists.
+                    using (FileStream fs = File.Create(path))
+                    {
+                        byte[] info = new UTF8Encoding(true).GetBytes(json);
+                        // Add some information to the file.
+                        fs.Write(info, 0, info.Length);
+                    }
+
+                    // Open the stream and read it back.
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            else
+            {
+
+                try
+                {
+
+                    string content;
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        content = reader.ReadToEnd();
+
+                        reader.Close();
+                    };
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        sw.WriteLine(content + " ," + " " + json);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+            }
+        }
+
     }
 }
